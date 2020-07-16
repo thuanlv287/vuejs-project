387e1c2453cfced41b9ca4bcaf71955668462c69
Thuan Le <thuanlv287@gmail.com>
2020-07-17 06:42:29 +0700

    Initialization project

---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <button type="button" class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data: () => ({}),
+  props: {
+    isOpen: Boolean
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <button type="button" class="btn btn-primary" v-on:click="toggleLogin">Login</button>
+    <LoginDialog :isOpen="isToggle" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +9,19 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import LoginDialog from "@/components/Login.vue";
 
 export default {
   name: "Home",
+  data: () => ({
+    isToggle: false
+  }),
+  methods: {
+    toggleLogin: () => {}
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginDialog
   }
 };
 </script>
